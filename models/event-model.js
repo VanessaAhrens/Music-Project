3e7d3d309d14f8fb9bc9cc4ef9d9a9ed8e48c0e0
Ahrens <ahrensv@media-saturn.com>
2019-10-03 11:56:53 +0200

    correction of typo in event-models

--- models/event-model.js
+++ models/event-model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema({
  name: { type: String },
  description: { type: String },
- Date: { type: Date },
+ date: { type: Date },
  owner: { type: Schema.Types.ObjectId, ref: 'User' },
  //reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' } ]
 })
